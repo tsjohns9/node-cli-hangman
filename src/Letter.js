5a6d5b016d36376fbbf94b79c89c31cf9535e2d9
Trevor Johnson <johnsontrevor55@gmail.com>
2018-04-16 15:12:51 -0700

    game is no longer case sensative.

--- src/Letter.js
+++ src/Letter.js
@@ -10,28 +10,31 @@ function Letter(letter) {
 // used to reveal the letter
 Letter.prototype.revealCharacter = function() {
   // auto reveals white space
-  if (this.letter === ' ') {
-    return ' ';
+  if (this.letter === " ") {
+    return " ";
   }
 
   // runs if the letter has not been revealed
   else if (!this.revealed) {
-    return '_';
+    return "_";
   }
 
   // If this runs, then the guess matches the letter and it gets revealed
   return this.letter;
-}
+};
 
 // updates the status of the letter to see if it has been guessed or not
 Letter.prototype.checkLetter = function(guess) {
-  // then sets this value to true so it can be revealed in the revealCharacter method
-  if (guess === this.letter) {
+  // Allows String.toUpperCase to be called on guess because guess is not initially a string. You get guess.toUpperCase() is not a function if you don't.
+  guess = guess || "";
+
+  // sets this value to true so it can be revealed in the revealCharacter method.  Check guess and the letter as upper case to ignore case.
+  if (guess.toUpperCase() === this.letter.toUpperCase()) {
     this.revealed = true;
   }
 
   // returns either an _, ' ', or the correctly guessed letter
   return this.revealCharacter();
-}
+};
 
 module.exports = Letter;
